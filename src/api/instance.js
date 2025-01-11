8fd0f1d4d5aa24966822ac9db51dee42fc4cf0df
jungsun <dlwjdtjs0111@gmail.com>
2025-01-12 01:16:11 +0900

    ✨ feat: 로그인 연결

--- src/api/instance.js
+++ src/api/instance.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
-import { applyInterceptors } from './interceptor';
+import axios from "axios";
+import { applyInterceptors } from "./interceptor";
 
 // .env로 숨긴 URL 주소
 // eslint-disable-next-line no-undef
-const BASE_URL = process.env.REACT_APP_BACKEND_SERVER_URL;
+const BASE_URL = import.meta.env.VITE_BACKEND_SERVER_URL;
 
 // Axios 기본 인스턴스
 const defaultInstance = axios.create({
@@ -11,14 +11,14 @@ const defaultInstance = axios.create({
   withCredentials: true, // Refresh 토큰 처리를 위한 옵션
 });
 
-// 인터셉터 적용
-// applyInterceptors(defaultInstance);
-
 // 추가 API 인스턴스
-// const createInstance = (baseInstance, path) => {
-//   const instance = axios.create(baseInstance.defaults);
-//   instance.defaults.baseURL += path;
-//   return instance;
-// };
+const createInstance = (baseInstance, path) => {
+  const instance = axios.create(baseInstance.defaults);
+  instance.defaults.baseURL += path;
+  return instance;
+};
 
-export { defaultInstance };
+const roomsInstance = createInstance(defaultInstance, "/rooms");
+applyInterceptors(roomsInstance);
+
+export { defaultInstance, roomsInstance };
--- src/api/interceptor.js
+++ src/api/interceptor.js
@@ -1,29 +1,27 @@
-//import { reissueToken } from './user/user';
-
-export const applyInterceptors = instance => {
+export const applyInterceptors = (instance) => {
   // 요청 인터셉터
   instance.interceptors.request.use(
-    config => {
-      const accessToken = localStorage.getItem('accessToken');
+    (config) => {
+      const accessToken = localStorage.getItem("accessToken");
       console.log(accessToken);
       // Access Token이 존재하고 유효할 경우 헤더에 추가
       if (accessToken) {
-        config.headers['Authorization'] = `Bearer ${accessToken}`;
+        config.headers["Authorization"] = `Bearer ${accessToken}`;
       }
       return config;
     },
-    error => {
-      console.error('❌ 요청 인터셉터 에러:', error.message || error);
+    (error) => {
+      console.error("❌ 요청 인터셉터 에러:", error.message || error);
       return Promise.reject(error);
     }
   );
 
   // 응답 인터셉터
   instance.interceptors.response.use(
-    response => {
+    (response) => {
       return response; // 성공적인 응답은 그대로 반환
     },
-    async error => {
+    async (error) => {
       const originalRequest = error.config;
 
       // Access Token 만료 처리 (401 Unauthorized)
@@ -35,26 +33,29 @@ export const applyInterceptors = instance => {
           //await reissueToken();
 
           // 재발급 후 원래 요청 다시 시도
-          const accessToken = localStorage.getItem('accessToken');
+          const accessToken = localStorage.getItem("accessToken");
           if (accessToken) {
-            originalRequest.headers['Authorization'] = `Bearer ${accessToken}`;
+            originalRequest.headers["Authorization"] = `Bearer ${accessToken}`;
           }
 
           return instance(originalRequest);
         } catch (refreshError) {
-          console.error('❌ 토큰 재발급 실패:', refreshError.message || refreshError);
+          console.error(
+            "❌ 토큰 재발급 실패:",
+            refreshError.message || refreshError
+          );
           // Refresh Token 실패 시 로그아웃 처리
-          localStorage.removeItem('accessToken');
+          localStorage.removeItem("accessToken");
           //document.cookie = 'refreshToken=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT;';
-          alert('세션이 만료되었습니다. 다시 로그인해주세요.');
-          window.location.href = '/login';
+          alert("세션이 만료되었습니다. 다시 로그인해주세요.");
+          window.location.href = "/login";
 
           return Promise.reject(refreshError);
         }
       }
 
       // 다른 에러 처리
-      console.error('❌ 응답 인터셉터 에러:', error.response || error.message);
+      console.error("❌ 응답 인터셉터 에러:", error.response || error.message);
       return Promise.reject(error);
     }
   );
